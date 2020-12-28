92cbfbf242b519f5a91073f5e445f1b7c35e2eaa
whatasoda <git@whatasoda.me>
2020-12-29 00:41:45 +0900

    feat: first step of abstruction

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -2,5 +2,5 @@ import wasm from '../pkg/index_bg.wasm';
 import pkg from '../pkg';
 
 Promise.all([pkg, wasm]).then(([pkg]) => {
-  pkg.greet();
+  pkg.start();
 });
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+mod geometry;
 mod utils;
 
+use crate::geometry::ShaderProgram;
+use webgl_matrix::Vec4;
+
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::WebGlRenderingContext;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +22,63 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("カッシーおはよう！");
+}
+
+#[repr(C)]
+struct Vertex {
+    position: Vec4,
+}
+
+#[wasm_bindgen]
+pub fn start() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let ctx = canvas
+        .get_context("webgl")?
+        .unwrap()
+        .dyn_into::<WebGlRenderingContext>()?;
+
+    let shader = ShaderProgram::<Vertex, ()>::new(
+        &ctx,
+        vec!["position"],
+        vec![],
+        vec![("position", 4)],
+        vec![],
+        r#"
+        attribute vec4 position;
+        void main() {
+            gl_Position = position;
+        }
+        "#,
+        r#"
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+        "#,
+    )?;
+    ctx.use_program(Some(&shader.program));
+
+    let vertices = vec![
+        Vertex {
+            position: [-0.7, -0.7, 0.5, 1.0],
+        },
+        Vertex {
+            position: [0.7, -0.7, 0.5, 1.0],
+        },
+        Vertex {
+            position: [0.0, 0.7, 0.5, 1.0],
+        },
+    ];
+    unsafe {
+        shader.vertex_buffer_data(&ctx, &vertices);
+    }
+
+    ctx.clear_color(0.0, 0.0, 0.0, 1.0);
+    ctx.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+    ctx.draw_arrays(WebGlRenderingContext::TRIANGLES, 0, vertices.len() as i32);
+    vertices.len();
+    Ok(())
 }
