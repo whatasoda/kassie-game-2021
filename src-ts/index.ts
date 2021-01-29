49b9eebe32d84bf857f702fa30ae97f635591f0a
whatasoda <git@whatasoda.me>
2021-01-29 21:23:36 +0900

    chore: add background load

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -2,7 +2,9 @@ import wasm from '../pkg/index_bg.wasm';
 import pkg from '../pkg';
 import '../assets/sample_texture.png';
 import '../assets/entities0.png';
+import '../assets/background.png';
 
 Promise.all([pkg, wasm]).then(([pkg]) => {
+  // eslint-disable-next-line no-console
   console.log(pkg.start());
 });
