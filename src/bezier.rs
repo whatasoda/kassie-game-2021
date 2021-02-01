0323a1ee902dcef10eb4815403844a2cc9413fc5
whatasoda <git@whatasoda.me>
2021-02-02 00:29:22 +0900

    chore: udpate bezier

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,4 +1,3 @@
-use num_traits::ToPrimitive;
 use std::iter::Iterator;
 
 #[inline]
@@ -27,131 +26,60 @@ pub fn bezier(t: f32, out: &mut [f32], p_0: &[f32], p_1: &[f32], p_2: &[f32], p_
 }
 
 pub struct Curve {
-    pub rate_coef: f32,
-    pub rate_1: f32,
-    pub rate_2: f32,
+    pub t_duration: f32,
+    pub t_p: Option<(f32, f32)>,
     pub p_0: [f32; 3],
     pub p_1: [f32; 3],
     pub p_2: [f32; 3],
     pub p_3: [f32; 3],
 }
 
-pub struct BezierTrajectory {
-    t: f32,
-    rate: f32,
-    loop_enabled: bool,
+pub struct BezierGroup {
     curves: Vec<Curve>,
     t_duration: f32,
-    t_hint: Vec<f32>,
 }
 
-impl BezierTrajectory {
-    pub fn new(loop_enabled: bool, curves: Vec<Curve>) -> Self {
-        let (t_duration, t_hint) = calc_t_duration_hint(&curves);
-        Self {
-            t: 0.,
-            rate: 0.1,
-            curves,
-            loop_enabled,
-            t_hint,
-            t_duration,
+impl BezierGroup {
+    pub fn new(curves: Vec<Curve>) -> Self {
+        let mut t_duration = 0.;
+        for curve in curves {
+            t_duration += curve.t_duration;
         }
+        Self { curves, t_duration }
     }
 
-    pub fn set_loop(&mut self, loop_enabled: bool) {
-        self.loop_enabled = loop_enabled;
-    }
-
-    /// duration: ms
-    /// stride: ms / count
-    pub fn set_rate_by_time(&mut self, duration: f32, stride: f32) {
-        self.rate = self.t_duration / duration * stride;
-    }
-
-    pub fn calc_point(&self, t: f32, rate: f32, loop_enabled: bool) -> Option<([f32; 3], f32)> {
-        calc_point(&self.curves, t, rate, loop_enabled)
-    }
-
-    pub fn calc_current_point(&self) -> Option<([f32; 3], f32)> {
-        self.calc_point(self.t, self.rate, self.loop_enabled)
-    }
-
-    pub fn next(&mut self) -> Option<[f32; 3]> {
-        match self.calc_current_point() {
-            Some((out, next_t)) => {
-                self.t = next_t;
-                Some(out)
-            }
-            None => None,
-        }
-    }
-
-    pub fn calc_point_by_time(&self, duration: f32, time: f32) -> Option<[f32; 3]> {
-        let raw = (time / duration * self.t_duration).max(0.);
-        let t = {
-            let mut base = 0.;
-            let t = self
-                .t_hint
-                .iter()
-                .zip(self.curves.iter())
-                .find_map(|(hint, curve)| {
-                    if raw < *hint {
-                        Some((raw - base) * curve.rate_coef)
-                    } else {
-                        base = *hint;
-                        None
-                    }
-                });
-            match t {
-                Some(t) => t,
-                None => return None,
-            }
-        };
-        self.calc_point(t, 0., false).and_then(|(out, _)| Some(out))
-    }
-}
-
-fn calc_point(
-    curves: &Vec<Curve>,
-    t: f32,
-    rate: f32,
-    loop_enabled: bool,
-) -> Option<([f32; 3], f32)> {
-    let mut next_t = t;
-    let (u, i) = {
-        let f = t.floor().max(0.);
-        let i = f.to_u32().unwrap() as usize;
-        let u = t - f;
-        let i = if i < curves.len() {
-            i
+    pub fn calc_point(&self, t: f32, loop_enabled: bool) -> Option<[f32; 3]> {
+        let t = if t <= self.t_duration {
+            t
         } else if loop_enabled {
-            next_t = u;
-            0
+            t % self.t_duration
         } else {
             return None;
         };
-        (u, i)
-    };
-    let curve = curves.get(i).unwrap();
-    next_t += curve.rate_coef * rate;
-    let u = {
-        let mut o = [0.];
-        bezier(u, &mut o, &[0.], &[curve.rate_1], &[curve.rate_2], &[1.]);
-        o[0]
-    };
-    let mut out = [0., 0., 0.];
-    bezier(u, &mut out, &curve.p_0, &curve.p_1, &curve.p_2, &curve.p_3);
-    Some((out, next_t))
-}
 
-fn calc_t_duration_hint(curves: &Vec<Curve>) -> (f32, Vec<f32>) {
-    let mut acc: f32 = 0.;
-    let t_hint: Vec<f32> = curves
-        .iter()
-        .map(|curve| {
-            acc += 1. / curve.rate_coef;
-            acc
+        let mut base = 0.0;
+        let curve = self.curves.iter().find_map(|curve| {
+            let next_base = base + curve.t_duration;
+            if t <= next_base {
+                let t = (t - next_base) / curve.t_duration;
+                Some((t, curve))
+            } else {
+                base = next_base;
+                None
+            }
+        });
+        curve.and_then(|(t, curve)| {
+            let t = match curve.t_p {
+                Some((t_p_1, t_p_2)) => {
+                    let mut o = [0.];
+                    bezier(t, &mut o, &[0.], &[t_p_1], &[t_p_2], &[1.]);
+                    o[0]
+                }
+                None => t,
+            };
+            let mut out = [0., 0., 0.];
+            bezier(t, &mut out, &curve.p_0, &curve.p_1, &curve.p_2, &curve.p_3);
+            Some(out)
         })
-        .collect();
-    (acc, t_hint)
+    }
 }
